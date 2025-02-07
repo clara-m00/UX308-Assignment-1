a91c30e1811a4350ec0d8c70a4c88051d7d6abcc
clara-m00 <144965262+clara-m00@users.noreply.github.com>
2025-02-07 01:12:41 +0000

    add testing for reply

--- reply.js
+++ reply.js
@@ -1,3 +1,14 @@
 export function reply(message){
-    return `${message} to you too`;
+    let reply = `${message} to you too`;
+    if(message.includes("dumplings")){
+        reply = "Sure! It will be $13.50"
+    }
+
+    if(message.includes("steak")){
+        reply = "Sure! It will be $15.00"
+    }
+    
+    return reply;
+    
+    
 }
