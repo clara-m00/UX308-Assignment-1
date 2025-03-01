4288a52cc939e947a478d93cdd374d43f3d953b1
clara-m00 <144965262+clara-m00@users.noreply.github.com>
2025-03-01 05:52:06 +0000

    added all states and specs

--- Order.js
+++ Order.js
@@ -3,22 +3,84 @@ export class Order {
       this.OrderState = {
         WELCOMING: () => {
           let aReturn = [];
-          this.stateCur = this.OrderState.RESERVING;
-          aReturn.push("Welcome to Rich's Acton Rapid Test.");
-          aReturn.push("Would you like to reserve a rapid test kit?");
+          aReturn.push("Welcome to Clara's Ramen House");
+          aReturn.push("Would you like to see the menu?");
+          this.stateCur = this.OrderState.MENU;
           return aReturn;
         },
-        RESERVING: (sInput) => {
+        MENU: (sInput) => {
+          let aReturn = [];
+          if (sInput.toLowerCase().startsWith('y')) {
+            aReturn.push("1 - Tonkotsu Ramen");
+            aReturn.push("2 - Shoyu Ramen");
+            aReturn.push("3 - Miso Ramen");
+            aReturn.push("4 - Cancel order");
+            aReturn.push("Please type the number of the desired item.");
+            this.stateCur = this.OrderState.SIZES;
+          } else {
+            aReturn.push("Thanks, come again!");
+            this.isDone = true;
+          }
+          return aReturn;
+        },
+        SIZES: (sInput) => {
+          let aReturn = [];
+          if (sInput === '1' || sInput === '2' || sInput === '3') {
+            aReturn.push("1 - Regular");
+            aReturn.push("2 - Large");
+            aReturn.push("Please type the number of the desired item.");
+            this.stateCur = this.OrderState.TOPPINGS;
+          } else if (sInput === '4') {
+            aReturn.push("Your order is cancelled. Thank you, come again!");
+            this.isDone = true;
+          } 
+          
+          else {
+            aReturn.push("Sorry, I couldn't understand your order. Please call us at 123-456-7890");
+            this.isDone = true;
+          }
+          return aReturn;
+        },
+        TOPPINGS: (sInput) => {
+          let aReturn = [];
+          if (sInput === '1' || sInput === '2') {
+            aReturn.push("1 - Egg");
+            aReturn.push("2 - Fish cakes");
+            aReturn.push("3 - Bok Choy");
+            aReturn.push("4 - Bamboo Shoot");
+            aReturn.push("Please type the number of the desired item.");
+          } else {
+            aReturn.push("Sorry, I couldn't understand your order. Please call us at 123-456-7890");
+            this.isDone = true;
+          }
+          this.stateCur = this.OrderState.UPSELL;
+          return aReturn;
+        },
+        UPSELL: (sInput) => {
+          let aReturn = [];
+          if (sInput === '1' || sInput === '2' || sInput === '3' || sInput === '4') {
+            aReturn.push("Would you like any add ons?");
+            aReturn.push("1 - Extra meat");
+            aReturn.push("2 - Extra green onion");
+            aReturn.push("3 - Pop");
+            aReturn.push("4 - No thank you");
+          } else {
+            aReturn.push("Sorry, I couldn't understand your order. Please call us at 123-456-7890");
+            this.isDone = true;
+          }
+          this.stateCur = this.OrderState.CHECKOUT;
+          return aReturn;
+        },
+        CHECKOUT: (sInput) => {
           let aReturn = [];
           this.isDone = true;
-          if (sInput.toLowerCase().startsWith('y')) {
-            aReturn.push(`Your rapid test is reserved under the phone number ${this.sFrom}`);
+          if (sInput === '1' || sInput === '2' || sInput === '3' || sInput === '4') {
             let d = new Date();
             d.setMinutes(d.getMinutes() + 120);
-            aReturn.push(`Please pick it up at 123 Tidy St., Acton before ${d.toTimeString()}`);
+            aReturn.push("Order confirmed.");
+            aReturn.push(`Please pick it up before ${d.toTimeString()}`);
           } else {
-            aReturn.push("Thanks for trying our reservation system");
-            aReturn.push("Maybe next time");
+            aReturn.push("Sorry, I couldn't understand your order. Please call us at 123-456-7890");
           }
           return aReturn;
         }
--- spec/order_spec.js
+++ spec/order_spec.js
@@ -4,20 +4,107 @@ describe("Tests all stages of an order", function() {
     it("test hello", function() {
         const oOrder = new Order("999-999-9999");
         const aResults = oOrder.handleInput("hello");
-        expect(aResults[0]).toBe("Welcome to Rich's Acton Rapid Test.");
+        expect(aResults[0]).toBe("Welcome to Clara's Ramen House");
     });
-    it("test yes", function() {
+    it("test menu", function() {
         const oOrder = new Order("999-999-9999");
         oOrder.handleInput("hello");
         const aResults = oOrder.handleInput("yes");
-        expect(aResults[0]).toBe("Your rapid test is reserved under the phone number 999-999-9999");
+        expect(aResults[0]).toBe("1 - Tonkotsu Ramen");
     });
-    it("test no", function() {
+    it("test no menu", function() {
         const oOrder = new Order("999-999-9999");
         oOrder.handleInput("hello");
         const aResults = oOrder.handleInput("no");
-        expect(aResults[0]).toBe("Thanks for trying our reservation system");
+        expect(aResults[0]).toBe("Thanks, come again!");
+    });
+    it("test valid menu item", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello");
+        oOrder.handleInput("yes");
+        const aResults = oOrder.handleInput("1");
+        expect(aResults[0]).toBe("1 - Regular");
+    });
+    it("test invalid menu item", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello");
+        oOrder.handleInput("yes");
+        const aResults = oOrder.handleInput("8");
+        expect(aResults[0]).toBe("Sorry, I couldn't understand your order. Please call us at 123-456-7890");
+    });
+    it("test cancel menu item", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello");
+        oOrder.handleInput("yes");
+        const aResults = oOrder.handleInput("4");
+        expect(aResults[0]).toBe("Your order is cancelled. Thank you, come again!");
+    });
+    it("test valid size", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello");
+        oOrder.handleInput("yes");
+        oOrder.handleInput("1");
+        const aResults = oOrder.handleInput("1");
+        expect(aResults[0]).toBe("1 - Egg");
+    });
+    it("test invalid size", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello");
+        oOrder.handleInput("yes");
+        oOrder.handleInput("1");
+        const aResults = oOrder.handleInput("7");
+        expect(aResults[0]).toBe("Sorry, I couldn't understand your order. Please call us at 123-456-7890");
+    });
+    it("test valid topping", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello");
+        oOrder.handleInput("yes");
+        oOrder.handleInput("1");
+        oOrder.handleInput("1");
+        const aResults = oOrder.handleInput("1");
+        expect(aResults[0]).toBe("Would you like any add ons?");
+    });
+    it("test invalid topping", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello");
+        oOrder.handleInput("yes");
+        oOrder.handleInput("1");
+        oOrder.handleInput("1");
+        const aResults = oOrder.handleInput("6");
+        expect(aResults[0]).toBe("Sorry, I couldn't understand your order. Please call us at 123-456-7890");
+    });
+    it("test valid upsell item", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello");
+        oOrder.handleInput("yes");
+        oOrder.handleInput("1");
+        oOrder.handleInput("1");
+        oOrder.handleInput("1");
+        const aResults = oOrder.handleInput("1");
+        expect(aResults[0]).toBe("Order confirmed.");
+    });
+    it("test invalid upsell item", function() {
+        const oOrder = new Order("999-999-9999");
+        oOrder.handleInput("hello");
+        oOrder.handleInput("yes");
+        oOrder.handleInput("1");
+        oOrder.handleInput("1");
+        oOrder.handleInput("1");
+        const aResults = oOrder.handleInput("7");
+        expect(aResults[0]).toBe("Sorry, I couldn't understand your order. Please call us at 123-456-7890");
     });
   });
   
-  
+  // one for hello
+  // one for menu
+  // one for no i dont want to see menu
+  // one for valid menu item
+  // one for invalid menu item
+  // one for cancel menu item
+  // one for valid size
+  // one for invalid size
+  // one for valid topping
+  // one for invalid topping
+  // one for valid upsell addon
+  // one for invalid upsell addmon
+ 
